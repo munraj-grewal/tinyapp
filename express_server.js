f86f3065d74f4935c28a68faaa5786683fee07cc
munraj <munrajgrewal12@gmail.com>
2021-01-18 23:34:20 +0000

    implemented the new url

--- express_server.js
+++ express_server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,7 +36,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", urls);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x100000).toString(16);
+};

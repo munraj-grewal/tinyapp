29b962f311d9a6d210203dfaafad1970b0ac1d57
munraj <munrajgrewal12@gmail.com>
2021-01-18 23:44:15 +0000

    implemented the new url

--- express_server.js
+++ express_server.js
@@ -12,6 +12,7 @@ const urlDatabase = {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body.longURL;
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
